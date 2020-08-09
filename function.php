de221e42f45f6d9506db5155a6f346cc7862c42b
藤原立暉 <r9.20important@icloud.com>
2020-08-09 12:50:15 +0900

    no message

--- function.php
+++ function.php
@@ -82,5 +82,137 @@ $err_msg = array();
 //================================
 // バリデーション関数
 //================================
+function validRequired($str,$key){
+    if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $str)){
+        global $err_msg;
+        $err_msg[$key] = MSG01;
+    }
+}
+function validEmail($str,$key){
+    if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $str)){
+        global $err_msg;
+        $err_msg[$key] = MSG02;
+    }
+}
 
+function validEmailDup($email){
+    global $err_msg;
+    //例外処理
+    try {
+      // DBへ接続
+      $dbh = dbConnect();
+      // SQL文作成
+      $sql = 'SELECT count(*) FROM users WHERE email = :email AND delete_flg = 0';
+      $data = array(':email' => $email);
+      // クエリ実行
+      $stmt = queryPost($dbh, $sql, $data);
+      // クエリ結果の値を取得
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+      //array_shift関数は配列の先頭を取り出す関数です。クエリ結果は配列形式で入っているので、array_shiftで1つ目だけ取り出して判定します
+      if(!empty(array_shift($result))){
+        $err_msg['email'] = MSG08;
+      }
+    } catch (Exception $e) {
+      error_log('エラー発生:' . $e->getMessage());
+      $err_msg['common'] = MSG07;
+    }
+  }
+  //バリデーション関数（同値チェック）
+function validMatch($str1, $str2, $key){
+    if($str1 !== $str2){
+      global $err_msg;
+      $err_msg[$key] = MSG03;
+    }
+  }
+  //バリデーション関数（最小文字数チェック）
+  function validMinLen($str, $key, $min = 6){
+    if(mb_strlen($str) < $min){
+      global $err_msg;
+      $err_msg[$key] = MSG05;
+    }
+  }
+  //バリデーション関数（最大文字数チェック）
+  function validMaxLen($str, $key, $max = 256){
+    if(mb_strlen($str) > $max){
+      global $err_msg;
+      $err_msg[$key] = MSG06;
+    }
+  }
+  //バリデーション関数（半角チェック）
+  function validHalf($str, $key){
+    if(!preg_match("/^[a-zA-Z0-9]+$/", $str)){
+      global $err_msg;
+      $err_msg[$key] = MSG04;
+    }
+  }
+
+
+
+  //================================
+// データベース
+//================================
+//DB接続関数
+function dbConnect(){
+    //DBへの接続準備
+    $dsn = 'mysql:dbname=tsumiage;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = 'root';
+    $options = array(
+      // SQL実行失敗時にはエラーコードのみ設定
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
+      // デフォルトフェッチモードを連想配列形式に設定
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      // バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
+      // SELECTで得た結果に対してもrowCountメソッドを使えるようにする
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    );
+    // PDOオブジェクト生成（DBへ接続）
+    $dbh = new PDO($dsn, $user, $password, $options);
+    return $dbh;
+  }
+  //SQL実行関数
+  //function queryPost($dbh, $sql, $data){
+  //  //クエリー作成
+  //  $stmt = $dbh->prepare($sql);
+  //  //プレースホルダに値をセットし、SQL文を実行
+  //  $stmt->execute($data);
+  //  return $stmt;
+  //}
+  function queryPost($dbh, $sql, $data){
+    //クエリー作成
+    $stmt = $dbh->prepare($sql);
+    //プレースホルダに値をセットし、SQL文を実行
+    if(!$stmt->execute($data)){
+      debug('クエリに失敗しました。');
+      debug('失敗したSQL：'.print_r($stmt,true));
+      $err_msg['common'] = MSG07;
+      return 0;
+    }
+    debug('クエリ成功。');
+    return $stmt;
+  }
+  function getUser($u_id){
+    debug('ユーザー情報を取得します。');
+    //例外処理
+    try {
+      // DBへ接続
+      $dbh = dbConnect();
+      // SQL文作成
+      $sql = 'SELECT * FROM users  WHERE id = :u_id AND delete_flg = 0';
+      $data = array(':u_id' => $u_id);
+      // クエリ実行
+      $stmt = queryPost($dbh, $sql, $data);
+  
+      // クエリ結果のデータを１レコード返却
+      if($stmt){
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+      }else{
+        return false;
+      }
+      
+    } catch (Exception $e) {
+      error_log('エラー発生:' . $e->getMessage());
+    }
+  //  return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
 ?>
--- signup.php
+++ signup.php
@@ -1,5 +1,57 @@
 <?php
+require('function.php');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「　ユーザー登録ページ　');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
 
+if(!empty($_POST)){
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $pass_re = $_POST['pass_re'];
+
+    validRequired($email,'email');
+    validRequired($pass,'pass');
+    validRequired($pass_re,'pass_re');
+
+    if(!empty($_POST)){
+        validEmail($email,'email');
+        validMaxLen($email,'email');
+        validMinLen($email,'email');
+        validEmailDup($email);
+
+        validHalf($pass,'pass');
+        validMaxLen($pass,'pass');
+        validMinLen($pass,'pass');
+        validMatch($pass, $pass_re, 'pass_re');
+
+        if(empty($err_msg)){
+            try{
+                $dbh = dbConnect();
+                $sql = 'INSERT INTO users (email,password,login_time,create_date) VALUES(:email,:pass,:login_time,:create_date)';
+          $data = array(':email' => $email, ':pass' => password_hash($pass, PASSWORD_DEFAULT),
+                        ':login_time' => date('Y-m-d H:i:s'),
+                        ':create_date' => date('Y-m-d H:i:s'));
+          // クエリ実行
+          $stmt = queryPost($dbh, $sql, $data);
+          
+
+          if($stmt){
+              $sesLimit = 60*60;
+              $_SESSION['login_date'] = time();
+              $_SESSION['login_limit'] = $sesLimit;
+
+              $_SESSION['user_id'] = $dbh->lastInsetId();
+              debug('セッション変数の中身：'.print_r($_SESSION,true));
+              header("Location:index.php");
+          }
+            }catch (Exception $e){
+                error_log('エラー発生：'.$e->getMessage());
+                $err_msg['common'] = MSG07;
+            }
+        }
+    }
+}
 ?>
 
 <?php
@@ -15,21 +67,13 @@ require('header.php');
     <form action="" method="post">
         <h2 class="title">新規登録</h2>
         <!-- 未記入あり -->
-        <div class="area-msg">
-        </div>
-        <label class="">
-            <input type="text" name="email" placeholder="メールアドレス" value="">
-        </label>
+        <span class="area-msg" style="color:red;"><?php if(!empty($err_msg['email'])) echo $err_msg['email']; ?></span>
+            <input type="text" name="email" placeholder="メールアドレス" value="<?php if(!empty($_POST['email'])) echo $_POST['email'];?>">
         <!-- 未記入あり -->
-        <div class="area-msg">
-        </div>
-        <label class="">
-            <input type="password" name="pass" placeholder="パスワード" value="">
-        </label>
-
-        <label>
-             <input type="password" name="pass_re" placeholder="パスワード(再入力)">
-           </label>
+        <span class="area-msg" style="color:red;"><?php if(!empty($err_msg['pass'])) echo $err_msg['pass']; ?></span>
+            <input type="password" name="pass" placeholder="パスワード" value="<?php if(!empty($_POST['pass'])) echo $_POST['pass'];?>">
+        <span class="area_msg" style="color:red;"><?php if(!empty($err_msg['pass_re'])) echo $err_msg['pass_re']; ?></span>
+             <input type="password" name="pass_re" placeholder="パスワード(再入力)" value="<?php if(!empty($_POST['pass_re'])) echo $_POST['pass_re'];?>">
             <div class="btn-container">
               <input type="submit" class="btn btn-mid" value="登録">
             </div>
